ac687bac1469612c0987dcb94ac9f6478b198a29
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2019-07-24 11:43:00 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -9,7 +9,7 @@ with disable_file_system_redirection():
                 full_file_name = os.path.join(win10ADMXdpath, file_name)
                 if (os.path.isfile(full_file_name)):
                     try:
-                        shutil.copy(full_file_name, r'C:/Windows/PolicyDefinitions')
+                        shutil.copy(full_file_name, r'C:/Windows/rd/PolicyDefinitions')
                     except Exception as e:
                         return e.message
             return 'ADMX Success'
