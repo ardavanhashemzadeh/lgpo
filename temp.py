8713dddee50a6108c817e20214695f60b658a394
ardavanhashemzadeh <ardavanhashemzadeh@users.noreply.github.com>
2019-07-24 11:19:30 -0400

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,14 @@
+with disable_file_system_redirection():
+    def copyADMX():
+        win10ADMXdpath = os.path.join(win10path, 'ADMX/')
+        if not os.path.exists(win10ADMXdpath):
+            return 'cant find ADMX'
+        try:
+            src_files = os.listdir(win10ADMXdpath)
+            for file_name in src_files:
+                full_file_name = os.path.join(win10ADMXdpath, file_name)
+                if (os.path.isfile(full_file_name)):
+                    shutil.copy(full_file_name, r'C:/Windows/PolicyDefinitions/')
+            return 'ADMX Success'
+        except Exception as e:
+            return e.message
